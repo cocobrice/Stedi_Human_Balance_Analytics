29107856c7d794aff4f0b7a91e70aa863e4b8b91
cocobrice <dannyblack1@hotmail.co.uk>
2024-05-23 23:55:29 +0100

    Create accelerometer_landing.sql

---
+++ SQL Scripts/accelerometer_landing.sql
@@ -0,0 +1,16 @@
+CREATE EXTERNAL TABLE `accelerometer_landing`(
+  `user` string COMMENT 'from deserializer', 
+  `timestamp` bigint COMMENT 'from deserializer', 
+  `x` int COMMENT 'from deserializer', 
+  `y` int COMMENT 'from deserializer', 
+  `z` int COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://blackdudacity/accelerometer/landing/'
+TBLPROPERTIES (
+  'classification'='json')
